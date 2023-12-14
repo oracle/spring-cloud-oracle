c3debd99b1e297f0da794baba1f878b848057653
jitkukum <jitendra.z.kumar@oracle.com>
2023-12-14 13:37:13 +0530

    make main branch empty till release plan

--- docs/generate-docs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-
-# Copyright (c) 2023, Oracle and/or its affiliates.
-# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
-
-# This command regenerates the docs after editing.
-# You'll have to run this after editing the src/main/asciidoc/README.adoc
-
-mvn clean install -Pasciidocs
--- spring-cloud-oci-samples/README.md
+++
@@ -1,7 +0,0 @@
-# Spring Cloud OCI - Samples
-
-Each code sample in this folder exemplifies how to use one Spring Framework with OCI.
-
-* [OCI Storage Service Sample](./spring-cloud-oci-storage-sample/)
-* [OCI Notification Service Sample](./spring-cloud-oci-notification-sample/)
-* [OCI Logging Service Sample](./spring-cloud-oci-logging-sample/)
