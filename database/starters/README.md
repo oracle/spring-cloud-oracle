ac7315a9ba16dd70e2192a704249cf172b7506cf
Anders Swanson <91502735+anders-swanson@users.noreply.github.com>
2024-08-15 12:24:17 -0700

    Oracle JSON starter (#101)
    * Oracle JSON starter
    
    ---------
    
    Signed-off-by: Anders Swanson <anders.swanson@oracle.com>

--- database/starters/README.md
+++ database/starters/README.md
@@ -4,8 +4,9 @@ Oracle Database Spring Starters implements Spring Boot Starters to facilitate th
 
 The following starters are provided:
 
-| Starter                                                              | Description                                                                     |
-|----------------------------------------------------------------------|---------------------------------------------------------------------------------|
-| [Oracle Spring Boot Starter UCP](oracle-spring-boot-starter-ucp)     | Autoconfigure UCP for Oracle Database, over the default Hikari Connection Pool. |
-| [Oracle Spring Boot Starter AQJMS](oracle-spring-boot-starter-aqjms) | Autoconfigure Oracle Database AQJMS Connections.                                |
+| Starter                                                                                    | Description                                                                     |
+|--------------------------------------------------------------------------------------------|---------------------------------------------------------------------------------|
+| [Oracle Spring Boot Starter UCP](oracle-spring-boot-starter-ucp)                           | Autoconfigure UCP for Oracle Database, over the default Hikari Connection Pool. |
+| [Oracle Spring Boot Starter AQJMS](oracle-spring-boot-starter-aqjms)                       | Autoconfigure Oracle Database AQJMS Connections.                                |
+| [Oracle Spring Boot Starter JSON Collections](oracle-spring-boot-starter-json-collections) | Autoconfiguration and utilities for JSON with Oracle Database                   |
 
---
+++ database/starters/oracle-spring-boot-starter-json-collections/src/test/java/com/oracle/spring/json/JsonCollectionsIT.java
@@ -0,0 +1,119 @@
+// Copyright (c) 2024, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+package com.oracle.spring.json;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.time.Duration;
+import java.util.List;
+import java.util.UUID;
+
+import com.oracle.spring.json.jsonb.JSONB;
+import com.oracle.spring.json.jsonb.JSONBRowMapper;
+import com.oracle.spring.json.jsonb.SODA;
+import com.oracle.spring.json.test.Student;
+import com.oracle.spring.json.test.StudentDetails;
+import oracle.jdbc.OracleTypes;
+import oracle.soda.OracleCollection;
+import oracle.soda.OracleDatabase;
+import oracle.soda.OracleDocument;
+import oracle.soda.rdbms.OracleRDBMSClient;
+import oracle.ucp.jdbc.PoolDataSource;
+import oracle.ucp.jdbc.PoolDataSourceFactory;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.oracle.OracleContainer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(classes = JsonCollectionsAutoConfiguration.class)
+@Testcontainers
+public class JsonCollectionsIT {
+    static OracleContainer oracleContainer = new OracleContainer("gvenzl/oracle-free:23.5-slim-faststart")
+            .withStartupTimeout(Duration.ofMinutes(2))
+            .withUsername("testuser")
+            .withPassword(("testpwd"));
+
+    static DataSource dataSource;
+    static JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    JSONB jsonb;
+    @Autowired
+    SODA soda;
+    @Autowired
+    OracleRDBMSClient client;
+
+    Student student1 = new Student(Student.newId(), "Bob", new StudentDetails(
+            "Computer Science",
+            3.33,
+            64
+    ));
+    Student student2 = new Student(Student.newId(), "Alice", new StudentDetails(
+            "Mathematics",
+            3.8,
+            80
+    ));
+
+    @BeforeAll
+    static void setUp() throws Exception {
+        oracleContainer.start();
+        PoolDataSource ds = PoolDataSourceFactory.getPoolDataSource();
+        ds.setConnectionFactoryClassName("oracle.jdbc.pool.OracleDataSource");
+        ds.setConnectionPoolName(UUID.randomUUID().toString());
+        ds.setURL(oracleContainer.getJdbcUrl());
+        ds.setUser(oracleContainer.getUsername());
+        ds.setPassword(oracleContainer.getPassword());
+        dataSource = ds;
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Test
+    void contextLoads() {}
+
+    @Test
+    void jsonbRowMapping() {
+        jdbcTemplate.execute("""
+            create table student (
+                data json
+            )
+            """);
+        jdbcTemplate.update(con -> {
+            PreparedStatement ps = con.prepareStatement("insert into student (data) values (?)");
+            byte[] oson = jsonb.toOSON(student1);
+            ps.setObject(1, oson, OracleTypes.JSON);
+            return ps;
+        });
+        RowMapper<Student> rowMapper = new JSONBRowMapper<>(jsonb, Student.class, 1);
+        List<Student> students = jdbcTemplate.query("select * from student", rowMapper);
+        assertThat(students).hasSize(1);
+        assertThat(students.get(0)).isEqualTo(student1);
+    }
+
+    @Test
+    void sodaMapping() throws Exception {
+        try (Connection conn = dataSource.getConnection()) {
+            OracleDatabase db = client.getDatabase(conn);
+            db.admin().createCollection("student_soda");
+            OracleCollection col = db.openCollection("student_soda");
+
+            OracleDocument document1 = soda.toDocument(db, student1);
+            OracleDocument document2 = soda.toDocument(db, student2);
+
+            col.insert(document1);
+            col.insert(document2);
+
+            OracleDocument found = col.find().filter("{\"name\":\"Alice\"}")
+                    .getOne();
+
+            Student student = soda.fromDocument(found, Student.class);
+            assertThat(student).isEqualTo(student2);
+        }
+    }
+}
