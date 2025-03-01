aee63c44e26d704048505135ec2eadd758050881
Andy Tael <andy.tael@yahoo.com>
2025-03-01 09:18:12 -0600

    Test skeleton

---
+++ recipes/hikari-ucp/src/test/java/oracle/com/cloud/recipes/hikariucp/ChangePropertyValueTest.java
@@ -0,0 +1,21 @@
+package oracle.com.cloud.recipes.hikariucp;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.properties.Assertions.properties;
+import static org.openrewrite.yaml.Assertions.yaml;
+
+class ChangeSpringPropertyValueTest implements RewriteTest {
+    
+    // @DocumentExample
+    // @Test
+    // void propFile() {
+    //     rewriteRun(
+    //       spec -> spec.recipe(new ChangeSpringPropertyValue("server.port", "8081", null, null,  null)),
+    //       properties("server.port=8080", "server.port=8081")
+    //     );
+    
+
+}
