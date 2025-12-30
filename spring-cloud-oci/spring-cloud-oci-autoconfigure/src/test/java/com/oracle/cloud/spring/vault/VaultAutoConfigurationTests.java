051669c90b7b1b935232879f1ab94788d7b5775f
Ryan Scheidter <ryan.scheidter@oracle.com>
2025-12-30 17:30:44 -0600

    Added VaultPropertySource happy path tests
    Signed-off-by: Ryan Scheidter <ryan.scheidter@oracle.com>

--- spring-cloud-oci/spring-cloud-oci-autoconfigure/src/test/java/com/oracle/cloud/spring/vault/VaultAutoConfigurationTests.java
+++ spring-cloud-oci/spring-cloud-oci-autoconfigure/src/test/java/com/oracle/cloud/spring/vault/VaultAutoConfigurationTests.java
@@ -2,23 +2,41 @@
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 package com.oracle.cloud.spring.vault;
 
+import com.oracle.bmc.secrets.SecretsClient;
+import com.oracle.bmc.secrets.model.Base64SecretBundleContentDetails;
+import com.oracle.bmc.secrets.model.SecretBundle;
+import com.oracle.bmc.secrets.requests.GetSecretBundleByNameRequest;
+import com.oracle.bmc.secrets.responses.GetSecretBundleByNameResponse;
+import com.oracle.bmc.vault.VaultsClient;
+import com.oracle.bmc.vault.model.SecretSummary;
+import com.oracle.bmc.vault.requests.ListSecretsRequest;
+import com.oracle.bmc.vault.responses.ListSecretsResponse;
 import com.oracle.cloud.spring.autoconfigure.TestCommonConfigurationBeans;
+import com.oracle.cloud.spring.autoconfigure.core.CredentialsProvider;
 import org.junit.jupiter.api.Named;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.Answers;
 import org.springframework.boot.autoconfigure.AutoConfigurations;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import java.util.Arrays;
+import java.util.Base64;
 import java.util.stream.Stream;
 
 import static com.oracle.cloud.spring.autoconfigure.TestCommonConfigurationBeans.assertBeanLoaded;
 import static com.oracle.cloud.spring.autoconfigure.TestCommonConfigurationBeans.assertBeanNotLoaded;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Named.named;
+import static org.mockito.AdditionalAnswers.answer;
+import static org.mockito.Mockito.*;
 
 public class VaultAutoConfigurationTests {
     private final String vaultIdProperty = "spring.cloud.oci.vault.vault-id=xyz";
@@ -73,5 +91,66 @@ public class VaultAutoConfigurationTests {
                     assertThat(ctx.getEnvironment().getPropertySources())
                             .doesNotHaveAnyElementsOfTypes(VaultPropertySource.class);
                 });
+    }
+
+    @ParameterizedTest
+    @ValueSource(booleans = {true, false})
+    void propertySourceAdded(boolean enableExplicitly) {
+        try(final var ignored1 = mockStatic(CredentialsProvider.class);
+            final var ignored2 = mockStatic(SecretsClient.class, Answers.RETURNS_DEEP_STUBS);
+            final var ignored3 = mockStatic(VaultsClient.class, Answers.RETURNS_DEEP_STUBS);
+            final var mockSecretsClient = mock(SecretsClient.class);
+            final var mockVaultsClient = mock(VaultsClient.class)
+        ) {
+            when(SecretsClient.builder().build(any())).thenReturn(mockSecretsClient);
+            when(VaultsClient.builder().build(any())).thenReturn(mockVaultsClient);
+
+            when(mockVaultsClient.listSecrets(any()))
+                .then(answer((ListSecretsRequest req) -> {
+                    assertEquals("abc", req.getVaultId());
+                    assertEquals("123", req.getCompartmentId());
+                    return newListSecretsResponse("secA", "secB");
+                }));
+
+            when(mockSecretsClient.getSecretBundleByName(any()))
+                .then(answer((GetSecretBundleByNameRequest req) -> {
+                    assertEquals("abc", req.getVaultId());
+                    //Return something arbitrary yet predictable for the contents:
+                    return newSecretBundleResponse(req.getSecretName() + "-val");
+                }));
+
+            contextRunner.withPropertyValues("spring.cloud.oci.vault.property-sources[0].vault-id=abc",
+                            enableExplicitly ? "spring.cloud.oci.vault.enabled=true" : "anything=else")
+                    .withInitializer(PROPERTY_SOURCE_INITIALIZER)
+                    .run(ctx -> {
+                        assertThat(ctx).hasNotFailed();
+                        assertThat(ctx.getEnvironment().getPropertySources())
+                                .hasAtLeastOneElementOfType(VaultPropertySource.class);
+                        assertEquals("secA-val", ctx.getEnvironment().getProperty("secA"));
+                        assertEquals("secB-val", ctx.getEnvironment().getProperty("secB"));
+                    });
+        }
+    }
+
+    @SuppressWarnings("SameParameterValue")
+    private static ListSecretsResponse newListSecretsResponse(String... secretNames) {
+        return ListSecretsResponse.builder()
+            .items(Arrays.stream(secretNames)
+                .map(secretName -> SecretSummary.builder().secretName(secretName).build())
+                .collect(toList())
+            )
+            .build();
+    }
+
+    private static GetSecretBundleByNameResponse newSecretBundleResponse(String secretContent) {
+        return GetSecretBundleByNameResponse.builder()
+            .secretBundle(SecretBundle.builder()
+                .secretBundleContent(Base64SecretBundleContentDetails.builder()
+                    .content(Base64.getEncoder().encodeToString(secretContent.getBytes(UTF_8)))
+                    .build()
+                )
+                .build()
+            )
+            .build();
     }
 }
