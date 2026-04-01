bea45cffcd7ee79150869f147c25effa896b7b3b
Mark Nelson <mark.x.nelson@oracle.com>
2026-04-01 14:48:59 -0400

    cors woopsie
    Signed-off-by: Mark Nelson <mark.x.nelson@oracle.com>

--- database/starters/oracle-spring-boot-starter-samples/oracle-spring-boot-sample-spatial/src/main/java/com/oracle/database/spring/spatial/LandmarkController.java
+++ database/starters/oracle-spring-boot-starter-samples/oracle-spring-boot-sample-spatial/src/main/java/com/oracle/database/spring/spatial/LandmarkController.java
@@ -4,7 +4,6 @@ package com.oracle.database.spring.spatial;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 public class LandmarkController {
     private final LandmarkService landmarkService;
