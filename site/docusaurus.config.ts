c40eb4660d3c8f10c371ea9f70651459111a96f4
Anders Swanson <anders.swanson@oracle.com>
2026-04-01 10:41:51 -0700

    site serving path
    Signed-off-by: Anders Swanson <anders.swanson@oracle.com>

--- site/docusaurus.config.ts
+++ site/docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://oracle.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/spring-cloud-oracle/',
+  baseUrl: '/spring-cloud-oracle/docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
