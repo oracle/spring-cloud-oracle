71b8844ad9aa2c1ac3e22e8b918540549074cd2f
Anders Swanson <anders.swanson@oracle.com>
2026-07-13 08:43:39 -0700

    update tests
    Signed-off-by: Anders Swanson <anders.swanson@oracle.com>

--- spring-ai-oracle/models/spring-ai-oracle/src/test/java/com/oracle/spring/ai/oracle/test/NoOpGenerativeAiInference.java
+++ spring-ai-oracle/models/spring-ai-oracle/src/test/java/com/oracle/spring/ai/oracle/test/NoOpGenerativeAiInference.java
@@ -7,18 +7,9 @@ package com.oracle.spring.ai.oracle.test;
 
 import com.oracle.bmc.Region;
 import com.oracle.bmc.generativeaiinference.GenerativeAiInference;
-import com.oracle.bmc.generativeaiinference.requests.ApplyGuardrailsRequest;
-import com.oracle.bmc.generativeaiinference.requests.ChatRequest;
-import com.oracle.bmc.generativeaiinference.requests.EmbedTextRequest;
-import com.oracle.bmc.generativeaiinference.requests.GenerateTextRequest;
-import com.oracle.bmc.generativeaiinference.requests.RerankTextRequest;
-import com.oracle.bmc.generativeaiinference.requests.SummarizeTextRequest;
-import com.oracle.bmc.generativeaiinference.responses.ApplyGuardrailsResponse;
-import com.oracle.bmc.generativeaiinference.responses.ChatResponse;
-import com.oracle.bmc.generativeaiinference.responses.EmbedTextResponse;
-import com.oracle.bmc.generativeaiinference.responses.GenerateTextResponse;
-import com.oracle.bmc.generativeaiinference.responses.RerankTextResponse;
-import com.oracle.bmc.generativeaiinference.responses.SummarizeTextResponse;
+import com.oracle.bmc.generativeaiinference.GenerativeAiInferencePaginators;
+import com.oracle.bmc.generativeaiinference.requests.*;
+import com.oracle.bmc.generativeaiinference.responses.*;
 
 public class NoOpGenerativeAiInference implements GenerativeAiInference {
 
@@ -48,6 +39,11 @@ public class NoOpGenerativeAiInference implements GenerativeAiInference {
     }
 
     @Override
+    public void enableDualStackEndpoints(boolean b) {
+
+    }
+
+    @Override
     public ApplyGuardrailsResponse applyGuardrails(ApplyGuardrailsRequest request) {
         throw unexpectedOciCall();
     }
@@ -68,6 +64,11 @@ public class NoOpGenerativeAiInference implements GenerativeAiInference {
     }
 
     @Override
+    public ListGuardrailVersionsResponse listGuardrailVersions(ListGuardrailVersionsRequest listGuardrailVersionsRequest) {
+        return null;
+    }
+
+    @Override
     public RerankTextResponse rerankText(RerankTextRequest request) {
         throw unexpectedOciCall();
     }
@@ -75,6 +76,11 @@ public class NoOpGenerativeAiInference implements GenerativeAiInference {
     @Override
     public SummarizeTextResponse summarizeText(SummarizeTextRequest request) {
         throw unexpectedOciCall();
+    }
+
+    @Override
+    public GenerativeAiInferencePaginators getPaginators() {
+        return null;
     }
 
     @Override
